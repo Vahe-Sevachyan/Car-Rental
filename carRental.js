56a939e7b2bdaa5cd8526df7bfbe18e1a53db1d1
Vahe Sevachyan <vscode13@gmail.com>
2021-05-01 12:28:40 -0700

    Car Rental
    Landing Page for Car Rental

---
+++ carRental.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("navBar");
+
+hamburger.addEventListener("click", ()=>{
+    navBar.classList.toggle("show");
+})
